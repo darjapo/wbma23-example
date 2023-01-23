30e04432be7204e96c167d7c41c864af7b41b73c
darjapo <darja.polevaja@metropolia.fi>
2023-01-23 09:53:21 +0200

    avatar in profile done

--- views/Profile.js
+++ views/Profile.js
@@ -1,13 +1,29 @@
-import React, {useContext} from 'react';
-import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
+import React, {useContext, useEffect} from 'react';
+import {StyleSheet, SafeAreaView, Text, Button, Image} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useTag} from '../hooks/ApiHooks';
+import {uploadsUrl} from '../utils/variables';
 
 const Profile = () => {
+  const {getFilesByTag} = useTag();
   const {setIsLoggedIn, user, setUser} = useContext(MainContext);
+  let avatar = '';
+
+  const loadAvatar = async () => {
+    const avatarArray = await getFilesByTag('avatar_' + user.user_id);
+    console.log('avatar', avatarArray.pop());
+    avatar = avatarArray.pop().filename;
+  };
+
+  useEffect(() => {
+    loadAvatar();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
+      <Image style={styles.image} source={{uri: uploadsUrl + avatar}} />
       <Text>Username: {user.username}</Text>
       <Text>Email: {user.email}</Text>
       <Text>Full name: {user.full_name}</Text>
@@ -34,6 +50,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     paddingTop: 40,
+  },
+  image: {
+    width: 200,
+    height: 300,
   },
 });
 export default Profile;
